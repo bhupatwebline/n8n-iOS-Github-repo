71bdb8373d46b0cf10400bba6f8292668cbfa0b5
pramitsinh <pramitmd@weblineapps.com>
2025-08-04 16:21:48 +0530

    initial setup

---
+++ iOSLocalization/iOSLocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSLocalizationApp.swift
+//  iOSLocalization
+//
+//  Created by iOS Pramit Dodiya  on 04/08/25.
+//
+
+import SwiftUI
+
+@main
+struct iOSLocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
